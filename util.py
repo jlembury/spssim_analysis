c967d9529f1069c01bf2eb71da53de23b0393723
Jessica Embury <jessica@gisjess.com>
2022-09-05 05:20:28 -0700

    Added function combine_csv_files().

--- util.py
+++ util.py
@@ -1,6 +1,9 @@
 # Jessica Embury, San Diego State University, Fall 2022
 # General functions that support the SpSSIM analysis
 
+import pandas as pd
+import os
+
 def Diff(li1, li2):
     """
     Find differences between 2 list objects
@@ -10,3 +13,16 @@ def Diff(li1, li2):
     """
     li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
     return li_dif
+
+
+def combine_csv_files(csv_dir, combo_csv_out=False, combo_csv=None):
+    files = os. listdir(csv_dir)
+    df = pd.read_csv(csv_dir + files[0])
+    for f in files[1:]:
+        tmp = pd.read_csv(csv_dir + f)
+        df = pd.concat([df, tmp], ignore_index=True)
+
+    if combo_csv_out:
+        df.to_csv(combo_csv, index=False)
+
+    return df
